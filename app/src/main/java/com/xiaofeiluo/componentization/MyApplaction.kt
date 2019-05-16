1541ef929ee366b48a8543817c48c3a484ab6695
wxxewx <419587118@qq.com>
2019-05-16 20:06:17 +0800

    update gradle buildscrip

--- app/src/main/java/com/xiaofeiluo/componentization/MyApplaction.kt
+++ app/src/main/java/com/xiaofeiluo/componentization/MyApplaction.kt
@@ -28,9 +28,9 @@ class MyApplaction : Application() {
     }
 
     private fun initRouter() {
-        if (BuildConfig.DEBUG) {           // 这两行必须写在init之前，否则这些配置在init过程中将无效
-            ARouter.openLog()     // 打印日志
-            ARouter.openDebug()  // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+        if (BuildConfig.DEBUG) {
+            ARouter.openLog()
+            ARouter.openDebug()
         }
         ARouter.init(this)
     }
