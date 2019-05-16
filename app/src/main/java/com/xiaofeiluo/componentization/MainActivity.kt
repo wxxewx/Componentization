95dc8c0cc76a0c13476e01f6952d848628d5710e
wxxewx <419587118@qq.com>
2019-05-16 21:52:21 +0800

    update app

--- app/src/main/java/com/xiaofeiluo/componentization/MainActivity.kt
+++ app/src/main/java/com/xiaofeiluo/componentization/MainActivity.kt
@@ -2,6 +2,8 @@ package com.xiaofeiluo.componentization
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioGroup
+import android.widget.Toast
 import com.alibaba.android.arouter.launcher.ARouter
 import com.xiaofeiluo.router.module1Home
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,9 +13,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        module1.setOnClickListener {
-            ARouter.getInstance().build(module1Home).navigation();
+        tab.setOnCheckedChangeListener { radioGroup: RadioGroup, i: Int ->
+            when (i) {
+                home.id -> Toast.makeText(this, "1", Toast.LENGTH_LONG).show()
+                wantToBuy.id -> Toast.makeText(this, "2", Toast.LENGTH_LONG).show()
+                me.id -> Toast.makeText(this, "3", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
